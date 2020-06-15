008b8c9d7a33e9af8465dd86c44f1ca24ba68021
Praneeth <praneethbedapudi@gmail.com>
2020-06-16 00:33:31 +0530

    add boilerplate
    Signed-off-by: Praneeth <praneethbedapudi@gmail.com>

---
+++ predictor.py
@@ -0,0 +1,11 @@
+import os
+import keras
+import pydload
+from keras_retinanet import models
+from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
+from keras_retinanet.utils.visualization import draw_box, draw_caption
+from keras_retinanet.utils.colors import label_color
+
+import cv2
+import numpy as np
+
